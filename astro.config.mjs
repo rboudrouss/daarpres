c02743e3ea039a4234b433e3917a6fe957cbd285
rboudrouss <r.boudrouss@gmail.com>
2026-03-30 00:04:59 +0200

    fix: add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from "astro/config";
 
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const owner = process.env.GITHUB_REPOSITORY_OWNER;
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: owner ? `https://${owner}.github.io` : undefined,
+  base: repo ? `/${repo}` : undefined,
+});
