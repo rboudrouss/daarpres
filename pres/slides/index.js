f6f365a29c85db5a794990fb97996e96960b7054
rboudrouss <r.boudrouss@gmail.com>
2026-03-29 13:43:36 +0200

    vibe coded slides

---
+++ pres/slides/index.js
@@ -0,0 +1,19 @@
+import slide01 from './01-title.html?raw';
+import slide02 from './02-why.html?raw';
+import slide03 from './03-population.html?raw';
+import slide04 from './04-hunting.html?raw';
+import slide05 from './05-defense.html?raw';
+import slide06 from './06-results.html?raw';
+import slide07 from './07-bilan.html?raw';
+import slide08 from './08-conclusion.html?raw';
+
+export const slides = [
+  slide01,
+  slide02,
+  slide03,
+  slide04,
+  slide05,
+  slide06,
+  slide07,
+  slide08,
+];
